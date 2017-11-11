6c0c09942c88941c7716965269e878d24a176ba8
Alberto Moreno <arbems@outlook.com>
2017-11-11 09:19:44 +0100

    Initial
    Initial Commit

---
+++ Example NLayer/Application.MainBoundedContext.DTO/Profiles/BankingProfile.cs
@@ -0,0 +1,36 @@
+﻿//===================================================================================
+// Microsoft Developer & Platform Evangelism
+//=================================================================================== 
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+//===================================================================================
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// This code is released under the terms of the MS-LPL license, 
+// http://microsoftnlayerapp.codeplex.com/license
+//===================================================================================
+namespace Microsoft.Samples.NLayerApp.Application.MainBoundedContext.DTO.Profiles
+{
+    using AutoMapper;
+    using Microsoft.Samples.NLayerApp.Application.MainBoundedContext.DTO;
+    using Microsoft.Samples.NLayerApp.Domain.MainBoundedContext.BankingModule.Aggregates.BankAccountAgg;
+
+    class BankingProfile
+        : Profile
+    {
+			public BankingProfile()
+			{
+				var config = new MapperConfiguration(cfg =>
+				{
+					cfg.CreateMap<BankAccountActivity, BankActivityDTO>();
+				});
+
+				Mapper.Initialize(cfg =>
+				{
+					cfg.CreateMap<BankAccount, BankAccountDTO>()
+					.ForMember
+							(dto => dto.BankAccountNumber, mc => mc.MapFrom(e => e.Iban));
+				});
+			}
+    }
+}
