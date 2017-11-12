c746d5432fb245c33e6906a89ca89e5edf05be8a
Alberto Moreno <arbems@outlook.com>
2017-11-12 20:33:13 +0100

    Developing AutoMapper 6.2
    Developing AutoMapper 6.2

--- Example NLayer/Infrastructure.Crosscutting.NetFramework/Adapter/AutomapperTypeAdapterFactory.cs
+++ Example NLayer/Infrastructure.Crosscutting.NetFramework/Adapter/AutomapperTypeAdapterFactory.cs
@@ -16,19 +16,19 @@
         public AutomapperTypeAdapterFactory()
         {
             //scan all assemblies finding Automapper Profile
-            var profiles = AppDomain.CurrentDomain
-                                    .GetAssemblies()
-                                    .SelectMany(a => a.GetTypes())
-                                    .Where(t => t.BaseType == typeof(Profile));
+            //var profiles = AppDomain.CurrentDomain
+            //                        .GetAssemblies()
+            //                        .SelectMany(a => a.GetTypes())
+            //                        .Where(t => t.BaseType == typeof(Profile));
 
-            Mapper.Initialize(cfg =>
-            {
-                foreach (var item in profiles)
-                {
-                    if (item.FullName != "AutoMapper.SelfProfiler`2")
-                        cfg.AddProfile(Activator.CreateInstance(item) as Profile);
-                }
-            });
+            //Mapper.Initialize(cfg =>
+            //{
+            //    foreach (var item in profiles)
+            //    {
+            //        if (item.FullName != "AutoMapper.SelfProfiler`2")
+            //            cfg.AddProfile(Activator.CreateInstance(item) as Profile);
+            //    }
+            //});
         }
 
         #endregion
